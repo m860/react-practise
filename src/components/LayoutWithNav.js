fd09cd0a8932f29c23b1ba2fb85c15bdc61b6fbf
jean <jean.ma.1986@gmail.com>
2017-08-01 20:48:21 +0800

    fix

--- src/components/LayoutWithNav.js
+++ src/components/LayoutWithNav.js
@@ -1,22 +1,33 @@
 import React from "react";
 import BaseComponent from "./BaseComponent.js";
-import Nav from './meta/Nav'
-import Layout from './meta/Layout'
-import PropTypes from 'prop-types'
-import config from 'config'
+import Nav from "./meta/Nav";
+import Layout from "./meta/Layout";
+import PropTypes from "prop-types";
+import config from "config";
+
+const styles = {
+	wrapper: {
+		display: "flex",
+		flex: 1,
+		overflow: "auto"
+	}
+};
 
 export default class LayoutWithNav extends BaseComponent {
-	static propTypes={
-		navOptions:PropTypes.object
+	static propTypes = {
+		navOptions: PropTypes.object
 	};
-	static defaultProps={
-		navOptions:config.navOptions
+	static defaultProps = {
+		navOptions: config.navOptions
 	};
+
 	render() {
 		return (
 			<Layout>
 				<Nav {...this.props.navOptions}/>
-				{this.props.children}
+				<div style={styles.wrapper}>
+					{this.props.children}
+				</div>
 			</Layout>
 		);
 	}
