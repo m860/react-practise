61e1497e0d7c3d9af5934a69fbc6afd3bd5f7a2c
jean <jean.ma.1986@gmail.com>
2017-09-14 15:57:32 +0800

    fix

--- update.js
+++
@@ -1,9 +0,0 @@
-var exec = require('child_process').exec;
-var pkg = require('./package.json');
-var deps = [];
-for (var key in pkg.dependencies) {
-	deps.push(key);
-}
-var command = "npm i --save " + deps.join(' ');
-console.log('command : ' + command);
-exec(command);
